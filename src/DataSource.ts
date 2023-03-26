5c2eb72743491294340fc4af6e7bdecf5b2b78a7
Allana Larissa <larissaleonel98@gmail.com>
2023-03-26 13:11:55 -0300

    changes of .env

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -3,10 +3,10 @@ import 'reflect-metadata'
 import { DataSource } from "typeorm";
 const port = process.env.DB_PORT as number | undefined
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: port,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+	type: 'postgres',
+	host: process.env.DB_HOST,
+	port: port,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME
 })
